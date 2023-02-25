167ae99136d93b1b4bfd15c5625c2133df3e7e99
notomo <notomo.motono@gmail.com>
2023-02-25 12:43:33 +0900

    Adjust doc gen

--- spec/lua/waitevent/doc.lua
+++ spec/lua/waitevent/doc.lua
@@ -3,44 +3,15 @@ local plugin_name = vim.env.PLUGIN_NAME
 local full_plugin_name = plugin_name .. ".nvim"
 
 local example_path = ("./spec/lua/%s/example.lua"):format(plugin_name)
-vim.api.nvim_exec("luafile " .. example_path, true)
+local example = util.read_all(example_path)
+local default_opttion_as_text = util.extract_variable_as_text("./lua/waitevent/option.lua", "default")
+example = example
+  .. ([[
 
-local get_default_option_as_text = function()
-  local path = [[./lua/waitevent/option.lua]]
-
-  local f = io.open(path, "r")
-  local str = f:read("*a")
-  f:close()
-
-  local query = vim.treesitter.query.parse_query(
-    "lua",
-    [[
-(variable_declaration
-  (assignment_statement
-    (variable_list
-        name: (_) @name (#match? @name "^default$")
-    )
-  )
-) @target
-]]
-  )
-
-  local parser = vim.treesitter.get_string_parser(str, "lua")
-  local trees = parser:parse()
-  local root = trees[1]:root()
-  local _, match = query:iter_matches(root, str, 0, -1)()
-
-  local target_node
-  for id, node in pairs(match) do
-    local captured = query.captures[id]
-    if captured == "target" then
-      target_node = node
-      break
-    end
-  end
-
-  return vim.treesitter.query.get_node_text(target_node, str)
-end
+-- all default options
+%s
+require("waitevent").editor(default)]]):format(default_opttion_as_text)
+util.execute(example)
 
 require("genvdoc").generate(full_plugin_name, {
   source = { patterns = { ("lua/%s/init.lua"):format(plugin_name) } },
@@ -68,17 +39,13 @@ require("genvdoc").generate(full_plugin_name, {
     {
       name = "EXAMPLES",
       body = function()
-        return util.help_code_block_from_file(example_path)
+        return util.help_code_block(example)
       end,
     },
   },
 })
 
 local gen_readme = function()
-  local f = io.open(example_path, "r")
-  local exmaple = f:read("*a")
-  f:close()
-
   local content = ([[
 # %s
 
@@ -92,10 +59,7 @@ This plugin provides the way to avoid nested nvim.
 
 ```lua
 %s
--- all default options
-%s
-require("waitevent").editor(default)
-```]]):format(full_plugin_name, exmaple, get_default_option_as_text())
+```]]):format(full_plugin_name, example)
 
   local readme = io.open("README.md", "w")
   readme:write(content)
